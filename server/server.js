1d1c838bf7e62d4d1f02c7b243fe2ec76e43b971
Andrew Tran <avtran23@gmail.com>
2017-04-12 11:06:46 -0700

    [setup]Server setup, listener and test route

--- server/server.js
+++ server/server.js
@@ -11,6 +11,15 @@ app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
 app.use(morgan('dev'))
 app.get('/test', (req, res) => {
-  res.status(200).send('Hello World!')
+  res.status(200).send('HAI WURLD')
 })
-app.use(express.static(path.join(__dirname, '/../app.build')))
+app.use(express.static(path.join(__dirname, '/../app/build')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/../client/index.html'))
+})
+
+let port = process.env.PORT || 3000
+app.listen(port, function() {
+  console.log('App listening on port:', port)
+})
