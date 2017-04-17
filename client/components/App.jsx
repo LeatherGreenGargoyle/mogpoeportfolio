9085d1b3fea57156a48c8df680889b7552519515
Andrew Tran <avtran23@gmail.com>
2017-04-17 14:08:03 -0700

    [setup]Add and refactor for ESLint and ES6

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-export default class App extends React.Component {
+class App extends Component {
   render() {
     return (
       <div>
         <h1>Hello World 4</h1>
       </div>
-    );
+    )
   }
 }
+
+export default App
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,39 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const SRC_DIR = path.resolve(__dirname, 'client/index.js')
 const BUILD_DIR = path.resolve(__dirname, 'app/build')
 
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: './client/index.html',
   filename: 'index.html',
-  inject: 'body'
+  inject: 'body',
 })
 
 module.exports = {
   entry: SRC_DIR,
   output: {
     path: BUILD_DIR,
-    filename: 'index_bundle.js'
+    filename: 'index_bundle.js',
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/},
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/}
-    ]
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+        },
+      },
+      { test: /\.jsx$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+        },
+      },
+    ],
   },
-  plugins: [HtmlWebpackPluginConfig]
+  plugins: [HtmlWebpackPluginConfig],
 }
